3f854701938737aa3bac745374fc89c356f01b3d
Fabian Froehlich <Fabian.Froehlich@mytum.de>
2012-11-19 15:45:39 +0100

    added initial point alignment with initial distribution

---
+++ 1D/InitOrgEnergy.m
@@ -0,0 +1,9 @@
+function W = InitOrgEnergy(Xp,DF,rstar,D0)
+    assert( isa( Xp , 'double'))
+    assert( isa( DF , 'double'))
+    assert( isa( rstar , 'double'))
+    assert( isa( D0 , 'double'))
+    Dp = exactDp(Xp,DF,rstar,D0);
+    Dpq = bsxfun(@min,Dp,Dp');
+    W = sum(sum(Dpq.^2.*V1(distm(Xp,Xp)./Dpq)));
+end
--- 1D/exactDp.m
+++ 1D/exactDp.m
@@ -1,16 +1,10 @@
-function Dp = calcDp(Xp,Xp_old,rstar,Dzero,rcp_old,f)
+function Dp = exactDp(Xp,DF,rstar,Dzero)
     assert( isa(Xp,'double'))
-    assert( isa(Xp_old,'double'))
+    assert( isa(DF,'double'))
     assert( isa(rstar,'double'))
     assert( isa(Dzero,'double'))
-    Dp = Dzero*ones(size(Xp,1),1);
-    DF = zeros(size(Xp,1),1);
-    for d=1:size(Xp_old,2)
-        DF = DF +(DiffOp(Xp,Xp_old,rcp_old,d)*f).^2;
-    end
-    DF=sqrt(DF);
+
     MF=monitor_function(DF,Dzero);
-    
     
     for j=1:size(Xp,1)
         Dp_old = -inf;
@@ -18,7 +12,7 @@ function Dp = calcDp(Xp,Xp_old,rstar,Dzero,rcp_old,f)
         k=0;
         while((((abs(Dp_old-Dp(j)))/Dp(j))>1e-2) && k < 15)
             Dp_old = Dp(j);
-            ind = (distm(Xp(j,:),Xp)<=rstar*Dp(j,:));
+            ind = (distm(Xp(j,:),Xp)<=rstar*Dp(j));
             if(sum(ind)>0)
                 Dp(j) = min(MF(ind));
                 k=k+1;
